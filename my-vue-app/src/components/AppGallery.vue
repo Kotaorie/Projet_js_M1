1aadc85caad19bc40984b398fe5a5e44d53011ef
nico servary <nicolas.servary123@gmail.com>
2024-12-09 12:48:52 +0100

    [Feat] adding thress js shaders

--- my-vue-app/src/components/AppGallery.vue
+++ my-vue-app/src/components/AppGallery.vue
@@ -1,86 +1,128 @@
 <template>
-    <div ref="cardContainer" class="w-[20vw] h-[80vh]"></div>
-    <h1>test</h1>
-  </template>
-  
-  <script>
-  import * as THREE from 'three';
-  
-  export default {
-    name: 'AppGallery',
-    mounted() {
-      this.initThreeJS();
-    },
-    methods: {
-        handleError(error) {
-            console.error('An error occurred:', error);
-        },
-      initThreeJS() {
-        console.log('initThreeJS');
-        const container = this.$refs.cardContainer;
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
-        const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-        renderer.setSize(container.clientWidth, container.clientHeight);
-        container.appendChild(renderer.domElement);
-  
-        // Charger la texture de l'image
-        const textureLoader = new THREE.TextureLoader();
-        textureLoader.crossOrigin = '';
-        const texture = textureLoader.load(require('@/assets/test.png'), () => {
-          renderer.render(scene, camera);
-        });
-  
-        // Création de la carte avec l'image
-        const geometry = new THREE.PlaneGeometry(1, 1.5); // Dimensions de la carte
-        const material = new THREE.MeshPhongMaterial({ map: texture, shininess: 100 }); // Matériau brillant avec texture
-        const card = new THREE.Mesh(geometry, material);
-        scene.add(card);
-  
-        // Lumière
-        const ambientLight = new THREE.AmbientLight(0x404040, 2); // lumière ambiante douce
-        scene.add(ambientLight);
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-        directionalLight.castShadow = true;
-        directionalLight.position.set(5, 5, 5).normalize();
-        scene.add(directionalLight);
-  
-        camera.position.z = 2;
+  <div ref="cardContainer" class="w-[20vw] h-[80vh]"></div>
+</template>
 
-        container.addEventListener('mousedown', (event) => {
-          container.style.cursor = 'grabbing';
-          const initialX = event.clientX;
-          const initialY = event.clientY;
-          const initialRotationX = card.rotation.x;
-          const initialRotationY = card.rotation.y;
-          const onMouseMove = (event) => {
-            const mouseX = (event.clientX - initialX) / 100;
-            const mouseY = (event.clientY - initialY) / 100;
-            card.rotation.x = initialRotationX + mouseY;
-            card.rotation.y = initialRotationY + mouseX;
-            renderer.render(scene, camera);
-          };
-          const onMouseUp = () => {
-            container.style.cursor = 'grab';
-            card.rotation.x = 0;
-            card.rotation.y = 0;
-            renderer.render(scene, camera);
-            document.removeEventListener('mousemove', onMouseMove);
-            document.removeEventListener('mouseup', onMouseUp);
-          };
-          document.addEventListener('mousemove', onMouseMove);
-          document.addEventListener('mouseup', onMouseUp);
-        });
-      }
-    }
-  };
-  </script>
-  
-  <style>
-  .card-container {
-    width: 50vh;
-    height: 100%;
-    position: relative;
+<script>
+import * as THREE from 'three';
+
+export default {
+  name: 'AppGallery',
+  mounted() {
+    this.initThreeJS();
+  },
+  methods: {
+    handleError(error) {
+      console.error('An error occurred:', error);
+    },
+    initThreeJS() {
+      const container = this.$refs.cardContainer;
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+      const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+      renderer.setSize(container.clientWidth, container.clientHeight);
+      container.appendChild(renderer.domElement);
+
+      // Charger la texture de l'image
+      const textureLoader = new THREE.TextureLoader();
+      const texture = textureLoader.load(require('@/assets/test.png'));
+
+      // Création de la carte avec l'image
+      const geometry = new THREE.PlaneGeometry(1, 1.5);
+      const material = new THREE.MeshPhongMaterial({ map: texture, shininess: 100 });
+      const card = new THREE.Mesh(geometry, material);
+      scene.add(card);
+
+      // Ajout d'un shaders
+      const vertexShader = `
+        varying vec2 vUv;
+        void main() {
+          vUv = uv;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+      `;
+
+      const fragmentShader = `
+        varying vec2 vUv;
+        uniform sampler2D uTexture;
+        uniform float time;
+        uniform float angle;
+
+        void main() {
+        
+          vec4 color = texture2D(uTexture, vUv);
+
+          float blueIntensity = color.b - color.r;
+          float shift = sin(time + color.r * 10.0 + vUv.x * 2.0  + angle * 10.0) * 0.2; 
+          color.r -= shift;
+          color.g -= shift;
+          color.b -= shift;
+
+          color = vec4(color.r, color.g, color.b, color.a);
+          
+
+          gl_FragColor = color;
+        }
+      `;
+
+      const shaderMaterial = new THREE.ShaderMaterial({
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+        uniforms: {
+          uTexture: { value: texture },
+          angle: { value: 0 }, // Initialisation de l'angle à 0
+        },
+      });
+
+      card.material = shaderMaterial;
+
+      const ambientLight = new THREE.AmbientLight(0x404040, 2);
+      scene.add(ambientLight);
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+      directionalLight.castShadow = true;
+      directionalLight.position.set(5, 5, 5).normalize();
+      scene.add(directionalLight);
+
+      camera.position.z = 2;
+
+      // Animations et interactions
+      const animate = () => {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+      };
+      animate();
+
+      container.addEventListener('mousemove', (event) => {
+        const x = container.clientWidth;
+        const y = container.clientHeight;
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+        const sensitivity = 2;
+        if (mouseX < 0 || mouseX > x || mouseY < 0 || mouseY > y) return;
+        
+        // Calcul de la rotation de la carte
+        card.rotation.y = ((mouseX / x) - 0.5) / sensitivity * Math.PI;
+        card.rotation.x = ((mouseY / y) - 0.5) / sensitivity * Math.PI;
+        shaderMaterial.uniforms.angle.value = card.rotation.x + card.rotation.y;
+
+        renderer.render(scene, camera);
+      });
+
+      container.addEventListener('mouseleave', () => {
+        card.rotation.x = 0;
+        card.rotation.y = 0;
+        shaderMaterial.uniforms.angle.value = 0; // Réinitialiser l'angle
+        renderer.render(scene, camera);
+      });
+    } 
+
   }
-  </style>
-  
+};
+</script>
+
+<style>
+.card-container {
+  width: 50vh;
+  height: 100%;
+  position: relative;
+}
+</style>
